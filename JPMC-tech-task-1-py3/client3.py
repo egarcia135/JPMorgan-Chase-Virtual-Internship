0585304d5c5ce3838639d946c6783ea241fede7e
Eduardo <egarci135@gmail.com>
2020-06-01 10:03:03 -0700

    Task 1

---
+++ JPMC-tech-task-1-py3/client3.py
@@ -0,0 +1,37 @@
+import urllib.request
+import time
+import json
+import random
+
+# Server API URLs
+QUERY = "http://localhost:8080/query?id={}"
+
+# 500 server request
+N = 500
+
+def getDataPoint(quote):
+
+	stock = quote['stock']
+	bid_price = float(quote['top_bid']['price'])
+	ask_price = float(quote['top_ask']['price'])
+	price = ask_price/bid_price
+	return stock, bid_price, ask_price, price
+
+def getRatio(price_a, price_b):
+	if(price_b == 0):
+		return 0
+	return price_a/price_b
+
+# Main
+if __name__ == "__main__":
+
+	# Query the price once every N seconds.
+	for _ in iter(range(N)):
+		quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+
+	
+		for quote in quotes:
+			stock, bid_price, ask_price, price = getDataPoint(quote)
+			print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+
+		print ("Ratio %s" % getRatio(price['ABC'], price['DEF']))
